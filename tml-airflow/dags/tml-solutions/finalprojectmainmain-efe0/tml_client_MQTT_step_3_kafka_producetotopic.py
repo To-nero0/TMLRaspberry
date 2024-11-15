f98705134ecfda9318593c8b4b45329809096425
To-nero0 <tonycoal20@gmail.com>
2024-11-14 21:13:40 -0500

    Update tml_client_MQTT_step_3_kafka_producetotopic.py

--- tml-airflow/dags/tml-solutions/finalprojectmainmain-efe0/tml_client_MQTT_step_3_kafka_producetotopic.py
+++ tml-airflow/dags/tml-solutions/finalprojectmainmain-efe0/tml_client_MQTT_step_3_kafka_producetotopic.py
@@ -5,9 +5,9 @@ import sys
 from datetime import datetime
 
 default_args = {
-  'mqtt_broker' : 'b526253c5560459da5337e561c142369.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
+  'mqtt_broker' : '28b92ad3dc2a4b9fac7b8de05498e422.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
   'mqtt_port' : '8883', # <<<******** Enter MQTT port i.e. 1883    
-  'mqtt_subscribe_topic' : 'tml/iot', # <<<******** enter name of MQTT to subscribe to i.e. encyclopedia/#
+  'mqtt_subscribe_topic' : 'tml/cybersecurity', # <<<******** enter name of MQTT to subscribe to i.e. encyclopedia/#
   'mqtt_enabletls' : '1', # << Enable TLS if connecting to a cloud cluster like HiveMQ
 }
 
